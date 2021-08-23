ad86c895bf4382af95ce0c7038225632d315f5ae
Wilfred <wilfredbayudan@gmail.com>
2021-08-22 19:45:01 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+    this.speak = () => `${this.name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+    this.speak = () => `${this.name} says woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+    this.speak = () => this.sex === 'male' ? `It's me! ${this.name}, the parrot!` : `${this.name} says squawk!`;
+  }
+}
